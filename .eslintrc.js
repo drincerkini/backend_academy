2d014a35c22f057fdbaa0bc6bc05b751411912a2
drincerkini <dc53912@ubt-uni.net>
2023-11-08 00:50:39 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['eslint:recommended', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+      // Add other ESLint rules here if needed
+    },
+  };
+  
---
+++ index.js
@@ -0,0 +1,4 @@
+const x = 3;
+const y = 4;
+
+console.log(x, y);
