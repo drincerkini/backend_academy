382ad10bd64df1d560aa1fc239ed3619945ec2ed
drincerkini <dc53912@ubt-uni.net>
2023-11-15 15:37:45 +0100

    getTodos completed

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const hello = 'Hello World';
+import fs from 'fs/promises';
 
-console.log(hello);
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+const getTodos = async() => {
+    try{
+        const data = await fetch(`${API_URL}`);
+        const dataJson = await data.json();
+        const dataToString = JSON.stringify(dataJson);
+        // console.log( dataToString);
+
+        await fs.writeFile('todosList.txt', dataToString);
+
+    }catch(error){
+        console.log('error' + error);
+    }
+}
+
+const getTodoById = () =>{
+
+}
+
+getTodos();
