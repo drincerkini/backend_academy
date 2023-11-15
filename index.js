abf2b6118ba24c9ef5e779979828ded47c9d5c29
drincerkini <dc53912@ubt-uni.net>
2023-11-15 16:34:19 +0100

    cleaned some code

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
-const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
-const getTodos = async() => {
-    try{
-        const data = await fetch(`${API_URL}`);
-        const dataJson = await data.json();
-        const dataToString = JSON.stringify(dataJson);
-        // console.log( dataToString);
+const getTodos = async () => {
+  try {
+    const data = await fetch(`${API_URL}`);
+    const dataJson = await data.json();
+    const dataToString = JSON.stringify(dataJson);
 
-        await fs.writeFile('todosList.txt', dataToString);
-        console.log('File Written successfuly');
-    }catch(error){
-        console.log('error' + error);
-    }
-}
+    await fs.writeFile("todosList.txt", dataToString);
+    console.log("File Written successfuly");
+  } catch (error) {
+    console.log("error" + error);
+  }
+};
 
-const getTodoById = async(id) =>{
-    if(!id) {
-        console.log('id is wrong!');
-        return;
-    }
+const getTodoById = async (id) => {
+  if (!id) {
+    console.log("id is wrong!");
+    return;
+  }
 
-    try{
-        const data = await fs.readFile('todosList.txt', { encoding: 'utf8' });
-        const todos = await JSON.parse(data);
-        const todo = todos.find(x => x.id === id)
-        console.log(todo);
-    }catch(err){
-        console.log('error' + err);
+  try {
+    const data = await fs.readFile("todosList.txt", "utf8");
+    const todos = JSON.parse(data);
+    const todo = todos.find((x) => x.id === id);
 
-    }
+    return todo;
+  } catch (err) {
+    console.log("error: " + err);
+  }
+};
 
-}
-
-// getTodos();
-getTodoById(3);
-// console.log(todoById)
+await getTodos();
+const todo = await getTodoById(1);
+console.log(todo);
