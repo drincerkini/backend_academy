999fe9856a9e842bccf18f9f4729554f97105213
drincerkini <dc53912@ubt-uni.net>
2023-11-18 19:23:18 +0100

    error handling middleware added

--- index.js
+++ index.js
@@ -1,79 +1,88 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-const PORT = 3000;
+const PORT = 3000
 
 // middlewares
-app.use(express.json());
-// midleware for loging each recived request
+app.use(express.json())
+
+// loging each recived request
 app.use((req, res, next) => {
-    console.log(`Recived new request: ${req.method} ${req.url}`);
-    next();
+  console.log(`Recived new request: ${req.method} ${req.url}`)
+  next()
+})
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error'
+  })
 })
 
 // Main Endpoint
-app.get("/", (req, res) => {
-  res.send({ message: "welome to express server!" });
-});
+app.get('/', (req, res) => {
+  res.send({ message: 'welome to express server!' })
+})
 
 // GET Endpoint
-app.get("/users", (req, res) => {
-  res.json(users);
-});
+app.get('/users', (req, res) => {
+  res.json(users)
+})
 
 // UPDATE Endpoint
-app.patch("/users/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
+app.patch('/users/:id', async (req, res) => {
+  const id = parseInt(req.params.id)
 
-  const index = users.findIndex((user) => user.id === id);
+  const index = users.findIndex(user => user.id === id)
 
   if (index === -1) {
-    return res.status(404).json({ error: "User not found!" });
+    return res.status(404).json({ error: 'User not found!' })
   }
 
   try {
-    const updatedUser = { ...users[index], ...req.body};
-    users[index] = updatedUser;
+    const updatedUser = { ...users[index], ...req.body }
+    users[index] = updatedUser
 
-    res.json({ message: "user updated succesfuly", user: updatedUser });
+    res.json({ message: 'user updated succesfuly', user: updatedUser })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
-});
+})
 
 // DELETE Endpoint
-app.delete("/users/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
+app.delete('/users/:id', async (req, res) => {
+  const id = parseInt(req.params.id)
 
   try {
-    let index = users.findIndex((user) => user.id === id);
+    let index = users.findIndex(user => user.id === id)
 
     if (index === -1) {
-      res.status(404).json({ error: "User does not exists!" });
+      res.status(404).json({ error: 'User does not exists!' })
     }
 
-    users.splice(index, 1);
-    res.status(200).json({ message: "User deleted successfuly" });
+    users.splice(index, 1)
+    res.status(200).json({ message: 'User deleted successfuly' })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`server listening on port: ${PORT}`);
-});
+  console.log(`server listening on port: ${PORT}`)
+})
 
 // users list
 const users = [
   {
     id: 1,
-    name: "Drin",
-    email: "dcerkini@gmail.com",
+    name: 'Drin',
+    email: 'dcerkini@gmail.com'
   },
   {
     id: 2,
-    name: "filan",
-    email: "filan@gmail.com",
-  },
-];
+    name: 'filan',
+    email: 'filan@gmail.com'
+  }
+]
